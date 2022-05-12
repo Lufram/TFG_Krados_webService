ef1627150efb215804dbc98841c44b5e2c27a636
Lufram <de.gregorio.madrid@gmail.com>
2022-05-12 12:16:10 +0200

    implement password encoder and user detaills

--- krados/src/main/java/com/edix/krados/KradosApplication.java
+++ krados/src/main/java/com/edix/krados/KradosApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -17,6 +19,12 @@ public class KradosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KradosApplication.class, args);
 	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 
 	@Bean
 	CommandLineRunner run(UserServiceImp userService) {
