091734092fec6c47580f6b24951ce29864fd14fd
Javier Baron <javier.baron@kenjo.io>
2022-05-20 23:44:48 +0200

    Last Update

---
+++ krados/src/test/java/com/edix/krados/KradosApplicationCategory.java
@@ -0,0 +1,30 @@
+package com.edix.krados;
+
+import com.edix.krados.controller.UserController;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+class KradosApplicationCategory {
+
+	@InjectMocks
+	private KradosApplication app;
+	@Autowired
+	public WebApplicationContext webApplicationContext;
+	@Autowired
+	public UserController userController;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	@Sql({"/category.sql"})
+	void addCategories(){
+	}
+
+}
--- krados/src/test/java/com/edix/krados/KradosApplicationRoles.java
+++ krados/src/test/java/com/edix/krados/KradosApplicationRoles.java
@@ -1,29 +1,17 @@
 package com.edix.krados;
 
 import com.edix.krados.controller.UserController;
-import com.edix.krados.form.RegisterForm;
-import com.edix.krados.repository.*;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.nio.charset.Charset;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class KradosApplicationTests {
+class KradosApplicationRoles {
 
 	@InjectMocks
 	private KradosApplication app;
@@ -37,18 +25,7 @@ class KradosApplicationTests {
 	}
 
 	@Test
-	@Sql({"/category.sql"})
-	void addCategories(){
-	}
-
-	@Test
 	@Sql({"/roles.sql"})
 	void addRoles(){
 	}
-
-	@Test
-	@Sql({"/product.sql"})
-	void addProducts(){
-	}
-
 }
